9f53852d6779f65f1e3cafdfea5b8716eb46a989
Kathrine Gibson <32083003+k-gibson@users.noreply.github.com>
2018-04-25 22:20:46 -0700

    Add files via upload

---
+++ SpaceImage.java
@@ -0,0 +1,28 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class SpaceImage extends JPanel {
+
+  private Image backgroundImage;
+
+  public SpaceImage(String fileName) throws IOException {
+    backgroundImage = ImageIO.read(new File(fileName));
+    Dimension size = new Dimension(backgroundImage.getWidth(null), backgroundImage.getHeight(null));
+    setPreferredSize(size);
+    setMinimumSize(size);
+    setMaximumSize(size);
+    setSize(size);
+    setLayout(null);
+  }
+
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+
+    // Draw the background image.
+    g.drawImage(backgroundImage, 0, 0, this);
+  }
+}

cd1917b79c3d0f5d7c2d81d41055f3420ea0bb60
Kathrine Gibson <32083003+k-gibson@users.noreply.github.com>
2018-04-28 17:04:39 -0700

    Update Space.java

--- Space.java
+++ Space.java
@@ -17,9 +17,12 @@ public class Space{
 // We can replace this class with the main GUI welcome screen with the rules and start game buttons.
 
   // Creates and then lets the user play a game of yahtzee
-  public static void main(String args[]) throws FileNotFoundException{
-    Game game = new Game();
-    game.playGame();
+  public static void main(String[] args) throws FileNotFoundException{
+	gameGUI newGame = new gameGUI();
+	newGame.setTitle("Race Through Space");
+    newGame.setDefaultCloseOperation(gameGUI.EXIT_ON_CLOSE);
+    newGame.setVisible(true);
   }
 
 }
+

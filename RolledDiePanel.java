de6e4179923816f71d49355d137f60f31240f873
Kathrine Gibson <32083003+k-gibson@users.noreply.github.com>
2018-04-29 17:07:42 -0700

    Add files via upload

---
+++ RolledDiePanel.java
@@ -0,0 +1,24 @@
+import javax.swing.JPanel;
+
+public class RolledDiePanel extends JPanel{
+	private JPanel mainPanel = new JPanel();
+	
+	private Player thisPlayer;
+	
+	public RolledDiePanel(Player player, int numberOfTurns){
+		thisPlayer = player;
+		setUpPanel();
+	}
+	
+	private void setUpPanel() {
+		//creates a JPanel with Gridlayout that displays the dice and what they rolled
+		
+				/*
+				 * add mercury die image
+				 * if playHand == which ever planets
+				 * 		display that planets name over the die
+				 * ^^^ for every die in every 
+				 * 
+				 */
+	}
+}

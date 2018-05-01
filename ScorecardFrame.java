dc2f89dcec3727bb241827391f369adbc691ce1e
adillinger <38118463+adillinger@users.noreply.github.com>
2018-04-30 20:41:25 -0700

    Add files via upload

--- ScorecardFrame.java
+++ ScorecardFrame.java
@@ -1,26 +1,20 @@
-/**
-* This class displays each players individual scorecard with their most current scores
-* CPSC 224-01, Spring 2018
-* Final Project
-* class Scorecard.java
-* @author Alana Dillinger
-* @version v1.0 5/4/2018
-*/
-
-
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+
 import java.awt.*;
+import java.awt.Graphics;
 import java.awt.geom.*;
 import java.awt.event.*;
-import java.util.Arrays;
 
-public class ScorecardFrame extends JFrame implements ActionListener{
+
+
+public class ScorecardFrame extends JFrame implements ActionListener {
 	private static final int defaultWidth = 1200;
     private static final int defaultHeight = 800;
     private static final int scoreLineHeight = defaultHeight/15;
     
-    private JButton nextButton = new JButton("Next");
-    private JPanel cardPanel = new JPanel(new CardLayout());
+    private JButton nextButton = new JButton("Done");
+    private JPanel cardPanel = new JPanel();
     
     public ScorecardFrame(int[] upperCardScores, int smallStraight, int largeStraight, int triForce, int totalScore) {
     	setSize(defaultWidth, defaultHeight);
@@ -64,86 +58,71 @@ public class ScorecardFrame extends JFrame implements ActionListener{
         cardTitle.setForeground(Color.white);
         title.setBackground(new java.awt.Color(40,23,35));
         title.add(cardTitle);
-        returnPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         returnPanel.add(title, BorderLayout.NORTH);
         
         //add lines to display the score on the screen
-        Graphics g = null; //************************************I know I need this, but idk how to initialize it.... Kate?
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(2f));
-        g2.setColor(Color.white);
-        
-        drawLines(g2);        
+        //Graphics g;
+        //paint(g); //************************************I know I need this, but idk how to initialize it.... Kate?
+              
         //convert integers in score array to strings to add them to labels
         String[] stringScores = new String[upperCardScores.length];
         for(int i=0; i < upperCardScores.length; i++) {
         	stringScores[i] = String.valueOf(upperCardScores[i]);
+        	System.out.println(upperCardScores.length);
         }
         //planet labels
-        JLabel mercury = new JLabel("Mercury"); JLabel mercuryScore = new JLabel(stringScores[0]);
-        JLabel venus = new JLabel("Venus"); JLabel venusScore = new JLabel(stringScores[1]);
-        JLabel mars = new JLabel("Mars"); JLabel marsScore = new JLabel(stringScores[2]);
-        JLabel jupiter = new JLabel("Jupiter"); JLabel jupiterScore = new JLabel(stringScores[3]);
-        JLabel saturn = new JLabel("Saturn"); JLabel saturnScore = new JLabel(stringScores[4]);
-        JLabel uranus = new JLabel("Uranus"); JLabel uranusScore = new JLabel(stringScores[5]);
-        JLabel neptune = new JLabel("Neptune"); JLabel neptuneScore = new JLabel(stringScores[6]);
-        returnPanel.add(mercury); returnPanel.add(mercuryScore);
-        returnPanel.add(venus); returnPanel.add(venusScore);
-        returnPanel.add(mars); returnPanel.add(marsScore);
-        returnPanel.add(jupiter); returnPanel.add(jupiterScore);
-        returnPanel.add(saturn); returnPanel.add(saturnScore);
-        returnPanel.add(uranus); returnPanel.add(uranusScore);
-        returnPanel.add(neptune); returnPanel.add(neptuneScore);
+        JPanel scoresPanel = new JPanel();
+        scoresPanel.setBackground(new java.awt.Color(40,23,35));
+        scoresPanel.setPreferredSize(new Dimension(defaultWidth,defaultHeight - 120));
         
-        //add planet labels in between lines
-        mercury.setLocation(200,scoreLineHeight-25); mercuryScore.setLocation(600,scoreLineHeight-25);
-        venus.setLocation(200,scoreLineHeight*2-25); venusScore.setLocation(600,scoreLineHeight*2-25);
-        mars.setLocation(200,scoreLineHeight*3-25); marsScore.setLocation(600,scoreLineHeight*3-25);
-        jupiter.setLocation(200,scoreLineHeight*4-25); jupiterScore.setLocation(600,scoreLineHeight*4-25);
-        saturn.setLocation(200,scoreLineHeight*5-25); saturnScore.setLocation(600,scoreLineHeight*5-25);
-        uranus.setLocation(200,scoreLineHeight*6-25); uranusScore.setLocation(600,scoreLineHeight*6-25);
-        neptune.setLocation(200,scoreLineHeight*7-25); neptuneScore.setLocation(600,scoreLineHeight*7-25);
+        String[] columnNames = {"What is being Scored",
+                                "Points Scored"
+                               };
         
-        //bonus score labels
-        JLabel smallStrght = new JLabel("Small Straight"); JLabel smallStrghtScore = new JLabel(String.valueOf(smallStraight));
-        JLabel largeStrght = new JLabel("Large Straight"); JLabel largeStrghtScore = new JLabel(String.valueOf(largeStraight));
-        JLabel triFrc = new JLabel("Tri Force"); JLabel triFrcScore = new JLabel(String.valueOf(triForce));
-        JLabel total = new JLabel("TOTAL SCORE"); JLabel ttlScore = new JLabel(String.valueOf(totalScore));
-        returnPanel.add(smallStrght);
-        returnPanel.add(largeStrght);
-        returnPanel.add(triFrc);
-        returnPanel.add(total);
+        Object[][] scores = {
+        	    {"Mercury", stringScores[0],},
+        	    {"Venus", stringScores[1],},
+        	    {"Mars", stringScores[2],},
+        	    {"Jupiter", stringScores[3],},
+        	    {"Saturn", stringScores[4],},
+        	    {"Uranus", stringScores[5],},
+        	    {"Neptune", stringScores[6],},
+        	    {"Small Straight", String.valueOf(smallStraight),},
+        	    {"Large Straight", String.valueOf(largeStraight),},
+        	    {"Tri Force", String.valueOf(triForce),},
+        	    {"Total Score", String.valueOf(totalScore),}
+        	};
         
-        //set locations of bonus scores between the lines. It might not be perfectly lined up. I kind of guessed on the math
-        // and I still can't test anything so I can't see it....
-        smallStrght.setLocation(200, scoreLineHeight*9-25); smallStrghtScore.setLocation(600, scoreLineHeight*9-25);
-        largeStrght.setLocation(200, scoreLineHeight*10-25); largeStrghtScore.setLocation(600, scoreLineHeight*10-25);
-        triFrc.setLocation(200, scoreLineHeight*11-25); triFrcScore.setLocation(600, scoreLineHeight*11-25);
-        total.setLocation(200, scoreLineHeight*13-25); ttlScore.setLocation(600, scoreLineHeight*13-25);
+        JTable table = new JTable(scores, columnNames);
+        table.setPreferredSize(new Dimension(defaultWidth - 500, defaultHeight));
+        table.setBackground(new java.awt.Color(40,23,35));
+        table.setFont(new Font("Krungthep",1,24));
+        table.setForeground(Color.white);
         
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+        table.getColumnModel().getColumn(0).setCellRenderer( centerRenderer );
+        table.getColumnModel().getColumn(1).setCellRenderer( centerRenderer );
+        
+        int rowHeight = 50;
+        table.setRowHeight(0,rowHeight);
+        table.setRowHeight(1,rowHeight);
+        table.setRowHeight(2,rowHeight);
+        table.setRowHeight(3,rowHeight);
+        table.setRowHeight(4,rowHeight);
+        table.setRowHeight(5,rowHeight);
+        table.setRowHeight(6,rowHeight);
+        table.setRowHeight(7,rowHeight);
+        table.setRowHeight(8,rowHeight);
+        table.setRowHeight(9,rowHeight);
+        table.setRowHeight(10,rowHeight);
+        table.setRowHeight(11,rowHeight);
+        
+        
+        scoresPanel.add(table);
+        returnPanel.add(scoresPanel,BorderLayout.NORTH);
+        returnPanel.setOpaque(true);
         return returnPanel; 
-    }
-    
-    private void drawLines(Graphics g2) {
-        g2.drawLine(0,0, defaultWidth,0);
-        g2.drawLine(0,scoreLineHeight*1, defaultWidth,scoreLineHeight*1);
-        g2.drawLine(0,scoreLineHeight*2, defaultWidth,scoreLineHeight*2);
-        g2.drawLine(0,scoreLineHeight*3, defaultWidth,scoreLineHeight*3);
-        g2.drawLine(0,scoreLineHeight*4, defaultWidth,scoreLineHeight*4);
-        g2.drawLine(0,scoreLineHeight*5, defaultWidth,scoreLineHeight*5);
-        g2.drawLine(0,scoreLineHeight*6, defaultWidth,scoreLineHeight*6);
-        g2.drawLine(0,scoreLineHeight*7, defaultWidth,scoreLineHeight*7);
-        g2.drawLine(0,scoreLineHeight*8, defaultWidth,scoreLineHeight*8);
-        g2.drawLine(0,scoreLineHeight*9, defaultWidth,scoreLineHeight*9);
-        g2.drawLine(0,scoreLineHeight*10, defaultWidth,scoreLineHeight*10);
-        g2.drawLine(0,scoreLineHeight*11, defaultWidth,scoreLineHeight*11);
-        g2.drawLine(0,scoreLineHeight*12, defaultWidth,scoreLineHeight*12);
-        g2.drawLine(0,scoreLineHeight*13, defaultWidth,scoreLineHeight*13);
-        g2.drawLine(0,scoreLineHeight*14, defaultWidth,scoreLineHeight*14);
-        g2.drawLine(0,scoreLineHeight*15, defaultWidth,scoreLineHeight*15);
-        g2.drawLine(0,scoreLineHeight*16, defaultWidth,scoreLineHeight*16);
-        
-        
     }
     
     public void actionPerformed(ActionEvent e)
